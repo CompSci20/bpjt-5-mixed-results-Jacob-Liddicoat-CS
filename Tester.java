41c8cb4f2e144ed16fc75d7ed57da3ebee1cd627
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-03-10 21:02:27 +0000

    Add 5 math problems

--- Tester.java
+++ Tester.java
@@ -6,4 +6,12 @@ public class Tester
   double d2 = 1004.128;
   int i1 = 12;
   int i2 = 18;
-
+  public static void main(String args[])
+   {
+    System.out.println(Problem 1: 57.2 * (i1 / i2) + 1); //1.0
+    System.out.println(Problem 2: 57.2 * ((double)i1 / i2) + 1); //39.13333333333333
+    System.out.println(Problem 3: 15 - i1 * (d1 * 3) + 4); //-1345.39999999999
+    System.out.println(Problem 4: 15 - i1 * (int)(d1 * 3) + 4); //-1337
+    System.out.println(Problem 5: 15 - i1 * ((int)d1 * 3) + 4);//-1313
+   }
+  }
